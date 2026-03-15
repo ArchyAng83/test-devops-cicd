99fd3145b98cfd2035ba7189bb40dff77226541d
ArchyAng83 <saltkeeper1@gmail.com>
2026-03-15 17:51:52 +0300

    added price for the product entity

--- TestDevOpsApp.Application/Entities/Product.cs
+++ TestDevOpsApp.Application/Entities/Product.cs
@@ -11,5 +11,6 @@ namespace TestDevOpsApp.Application.Entities
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
     }
 }
