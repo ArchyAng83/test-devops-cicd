dd1cdbebc1b080cd340f15286ab2c928e4b3a381
ArchyAng83 <saltkeeper1@gmail.com>
2026-03-15 18:03:12 +0300

    added price for the product entity1

--- TestDevOpsApp.Domain/Entities/Product.cs
+++ TestDevOpsApp.Domain/Entities/Product.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestDevOpsApp.Application.Entities
+namespace TestDevOpsApp.Domain.Entities
 {
     public class Product
     {
