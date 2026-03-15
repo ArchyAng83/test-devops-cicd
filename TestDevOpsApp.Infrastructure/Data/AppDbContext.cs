025547505a4c8e686f5f07386b0bea1a7fa1fb9f
ArchyAng83 <saltkeeper1@gmail.com>
2026-03-15 18:08:57 +0300

    added db context

---
+++ TestDevOpsApp.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDevOpsApp.Domain.Entities;
+
+namespace TestDevOpsApp.Infrastructure.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
